a8d0d71c3739e256409ba1ec13707d1329116e13
lilico9 <143882709+lilico9@users.noreply.github.com>
2026-09-22 19:02:52 +0800

    1

--- src/app/douban/page.tsx
+++ src/app/douban/page.tsx
@@ -126,7 +126,7 @@ function DoubanPageClient() {
       setPrimarySelection('最近热门');
       setSecondarySelection('show');
     } else if (type === 'anime') {
-      setPrimarySelection('每日放送');
+      setPrimarySelection('番剧');
       setSecondarySelection('全部');
     } else if (isTagPage) {
       setPrimarySelection('热门');
@@ -422,8 +422,6 @@ function DoubanPageClient() {
           if (isTagPage) {
             // 标签页（短剧/纪录片）
             data = await getTagPageData(currentPage * 25);
-          } else if (type === 'anime' && primarySelection === '番剧') {
-            // 每日放送模式下，不进行数据请求，返回空数据
           } else if (type === 'anime') {
             data = await getDoubanRecommends({
               kind: primarySelection === '番剧' ? 'tv' : 'movie',
@@ -722,7 +720,7 @@ function DoubanPageClient() {
                     year={item.year}
                     type={type === 'movie' ? 'movie' : ''} // 电影类型严格控制，tv 不控
                     isBangumi={
-                      type === 'anime' && primarySelection === '每日放送'
+                      type === 'anime' && primarySelection === '番剧'
                     }
                   />
                 </div>
